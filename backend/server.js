dc08fb19acc3614bba5150fcd1e1ffeadb6fa722
Rohit Goyal <163022120+Mr-Rohit2006@users.noreply.github.com>
2026-05-20 17:40:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
